a70bba467a2eaf77f7798d24ea4e8249e8db947b
dspopravko <demid1498@gmail.com>
2023-01-10 14:03:41 +0400

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import cors from 'cors'
 import express, {Request, Response} from 'express'
-import {createTransport, getTestMessageUrl} from 'nodemailer'
+import {createTransport} from 'nodemailer'
 import dotenv from 'dotenv'
+
 dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,38 +17,28 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.post('/', async (req: Request, res: Response) => {
-	// await sendEmail(req.body)
-	console.log(process.env.PRIVATE_KEY)
+	const emailRes = await sendEmail(req.body)
 	console.log(req.body)
-	res.send(req.body)
+	res.status(200).send(emailRes)
 })
 
-app.listen(port, () => {
-	console.log(`Mail app listening on port ${port}`)
-})
+app.listen(port)
 
 const sendEmail = async (mail: mailType) => {
-
-	let info = await transporter.sendMail({
+	console.log('send email')
+	return await transporter.sendMail({
 		from: 'dspopravko-service', // sender address
-		to: "demid1498@gmail.com", // list of receivers
-		subject: `Message from ${mail.name}`, // Subject line
+		to: process.env.MAIL_RECEIVER, // list of receivers
+		subject: `Message from: ${mail.name}`, // Subject line
 		text: `Body: ${mail.message}`, // plain text body
 	});
-
-	console.log("Message sent: %s", info.messageId);
-	// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-	// Preview only available when sending through an Ethereal account
-	console.log("Preview URL: %s", getTestMessageUrl(info));
-	// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-let transporter = createTransport({
+const transporter = createTransport({
 	service: 'gmail',
 	auth: {
-		user: process.env.GMAIL_USER_NAME, // generated ethereal user
-		pass: process.env.GMAIL_PASSWORD, // generated ethereal password
+		user: process.env.GMAIL_USER_NAME,
+		pass: process.env.GMAIL_PASSWORD,
 	},
 });
 
